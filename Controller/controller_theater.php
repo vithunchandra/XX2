34248932417a20a2e98c401378533a342ee004f0
dragoox <datawolfxxx4@gmail.com>
2022-11-21 17:15:08 +0700

    displaying seat

--- Controller/controller_theater.php
+++ Controller/controller_theater.php
@@ -5,15 +5,45 @@
       $theater = $_GET['theater'];
       $tanggal = $_GET['tanggal'];
       $movieNow = $_GET['film'];
+      $sesi = $_GET['sesi'];
 
-      echo $theater . " " . $tanggal . " " . $movieNow;
+      $sizeNow = fetch("Select width,height from theater where id_theater = " . $theater);
+
+      $seat = [];
+      for($i = 0;$i < $sizeNow[0]['height'];$i++) {
+        for($j = 0;$j < $sizeNow[0]['width'];$j++)  {
+          //nanti tambahi pengecekan apakah seat sudah diambil di db
+          $seat[$i][$j] = 1;
+        }
+      }
+      echo json_encode($seat);
+      //echo $theater . " " . $tanggal . " " . $movieNow . " " . $sesi;
     }
 
     if(isset($_GET['get_all_broadcast_date'])) {
-        $sql = "select s.broadcast_date as tgl from theater_schedule as ts,schedule as s where ts.id_schedule = s.id_schedule and 
-        ts.id_theater = ".$_GET['theater']." and s.id_film = ".$_GET['film'].";";
+        $sql = "select distinct s.broadcast_date as tgl from theater_schedule as ts,schedule as s where ts.id_schedule = s.id_schedule and 
+        ts.id_theater = ".$_GET['theater']." and s.id_film = ".$_GET['film']." and ts.status = 1;";
 
         $result = fetch($sql);
         echo json_encode($result);
     }
+
+    if(isset($_GET['get_all_session'])) {
+      $sql = "select ses.id_session as id,ses.session_start as starts,ses.session_end as ends from theater_schedule as ts,schedule as s,session as ses where ts.id_schedule = s.id_schedule and ses.id_session = s.id_session and 
+      ts.id_theater = ".$_GET['theater']." and s.id_film = ".$_GET['film']." and s.broadcast_date = ".$_GET['date']." and ts.status = 1;";
+      
+      // echo $sql;
+      $result = fetch($sql);
+      echo json_encode($result);
+    } 
+
+    if(isset($_GET['buy'])) {
+      $theater = $_GET['theater'];
+      $tanggal = $_GET['tanggal'];
+      $movieNow = $_GET['film'];
+      $sesi = $_GET['sesi'];
+      $seat = json_decode($_GET['seat']);
+      // insert transaction and check money
+    }
+
 ?>
--- order.php
+++ order.php
@@ -46,22 +46,33 @@
   
   <div id="pilih_tgl" >
     <h3>Beli Tiket Untuk Tanggal</h3>
-    <select name="tanggal" id="tanggal">
+    <select id="tanggal">
     </select>
-    
   </div>
 
+  <div id="pilih_sesi" >
+    <h3>Sesi</h3>
+    <select id="sesi">
+    </select>
+  </div>
+
+  <button onclick="show_seat()">Ok</button>
+
+  <div id="av_seat"></div>
 
   <h3>Jumlah Tiket:</h3>
-  <input id = 'jum_tiket' type="text">
-  <button onclick="show_seat()">Ok</button>
+  <input disabled value = 0 id = 'jum_tiket' type="text">
+
+  <button onclick="buy()">Buy!</button>
+
 </body>
 </html>
 
 <script>
   var theater = "";
+  var seat = [];
   var jumlah_tiket = 0;
-  var choosen_seat = -1;
+  var choosen_seat = [];
 
   function update_pilih_tgl() {
     theater = document.getElementById('theater').value;
@@ -74,7 +85,7 @@
 
           var pilih_tgl = document.getElementById('pilih_tgl');
           tempHTML = "<h3>Beli Tiket Untuk Tanggal</h3>";
-          tempHTML += "<select>";
+          tempHTML += "<select id = 'tanggal' onchange = 'update_pilih_sesi()'>";
 
           for(var i = 0;i < rows.length;i++) {
             tempHTML += "<option>"+rows[i]['tgl']+"</option>";
@@ -82,31 +93,97 @@
           tempHTML += "</select>";
           
           pilih_tgl.innerHTML = tempHTML;
+
+          update_pilih_sesi();
         }
     };
     xhttp.open("GET", "Controller/controller_theater.php?get_all_broadcast_date=1&theater="+theater + "&film="+movieNow   );
     xhttp.send();
   }
 
-      
-  
-  function show_seat() {
+  function update_pilih_sesi() {
     theater = document.getElementById('theater').value;
-    jumlah_tiket = document.getElementById('jum_tiket').value;
-    tanngal = document.getElementById('tanngal').value;
-    movieNow = <?="'".$movie['id_film']."'"?>
+    var tgl_choosen = document.getElementById('tanggal').value;
     
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var responseText = xhttp.responseText;
+          var rows = JSON.parse(responseText);
+
+          var pilih_sesi = document.getElementById('sesi');
+          tempHTML = "<h3>Sesi</h3>";
+          tempHTML += "<select id = 'sesi'>";
+
+          for(var i = 0;i < rows.length;i++) {
+            tempHTML += "<option value = "+rows[i]['id']+">"+rows[i]['starts'] + " - " + rows[i]['ends'] +"</option>";
+          }
+          tempHTML += "</select>";
           
-          console.log(responseText);
+          pilih_sesi.innerHTML = tempHTML;
         }
     };
-    xhttp.open("GET", "Controller/controller_theater.php?get_seat=1&theater="+theater+"&tanggal='"+tanngal+"'&film="+movieNow   );
+    
+    xhttp.open("GET", "Controller/controller_theater.php?get_all_session=1&theater="+theater + "&film="+movieNow +"&date='"+tgl_choosen+"'"  );
+    xhttp.send();
+  }
+
+      
+  
+  function show_seat() {
+    choosen_seat = [];
+    jumlah_tiket = 0;
+
+    theater = document.getElementById('theater').value;
+    tanngal = document.getElementById('tanggal').value;
+    sesi = document.getElementById('sesi').value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var responseText = xhttp.responseText;
+          var seatRep = JSON.parse(responseText);
+          seat = seatRep;
+
+          var tempHTML = "";
+          for(var i = 0;i < seat.length;i++) {
+            for(var j = 0;j < seat[i].length;j++) {
+              tempHTML = tempHTML + "<button onclick = 'choose_seat(this)' value = '"+i+";"+j+"'>1</button>";
+            }
+            tempHTML = tempHTML + "<br>";
+          }
+          document.getElementById('av_seat').innerHTML = tempHTML;
+        }
+    };
+    xhttp.open("GET", "Controller/controller_theater.php?get_seat=1&theater="+theater+"&tanggal='"+tanngal+"'&film="+movieNow + "&sesi=" + sesi  );
     xhttp.send();
   }
   
+  function choose_seat(e) {
+    if(e.innerHTML == 1) {
+      jumlah_tiket = jumlah_tiket + 1;
+      document.getElementById('jum_tiket').value = jumlah_tiket;  
+      e.innerHTML = 0;
+      var idx_i = parseInt(e.value.split(';')[0]);
+      var idx_j = parseInt(e.value.split(';')[1]);
+      choosen_seat.push([idx_i,idx_j]);
+    }
+  }
+
+  function buy() {
+    theater = document.getElementById('theater').value;
+    tanngal = document.getElementById('tanggal').value;
+    sesi = document.getElementById('sesi').value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", "Controller/controller_theater.php?buy=1&seat="+JSON.stringify(choosen_seat)+"&theater="+theater + "&tanggal='"+tanngal+"'&film="+movieNow + "&sesi=" + sesi  );
+    xhttp.send();
+  }
+
   update_pilih_tgl();
 </script>
